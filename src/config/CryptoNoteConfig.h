311c8cf2c8632c8473dca0e5f73887a1b9895fbb
Chris T <37755722+hashratez@users.noreply.github.com>
2018-09-27 16:03:07 -0400

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -73,7 +73,7 @@ const char     GENESIS_COINBASE_TX_HEX[]                     = "011401ff00018081
 /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
    You can get this value by doing "print_block 2" in TurtleCoind. It is used to know what timestamp
    to import from when the block height cannot be found in the node or the node is offline. */
-const uint64_t GENESIS_BLOCK_TIMESTAMP                       = 1538038094;
+const uint64_t GENESIS_BLOCK_TIMESTAMP                       = 1538046384;
 
 const size_t   CRYPTONOTE_REWARD_BLOCKS_WINDOW               = 100;
 const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 100000; //size of block (bytes) after which reward for block calculated using block size
@@ -240,6 +240,11 @@ const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 const char* const SEED_NODES[] = {
   "node1.aescoin.io:18111", //node1
   "node2.aescoin.io:18111", //node2
-  "node3.aescoin.io:18111", //node3  
+  "node3.aescoin.io:18111", //node3
+  "node4.aescoin.io:18111", //node4
+  "lax1.aescoin.io:18111", //lax1
+  "dfw1.aescoin.io:18111", //dfw1
+  "ord1.aescoin.io:18111", //ord1
+  "ny1.aescoin.io:18111", //ny1 
 };
 } // CryptoNote
